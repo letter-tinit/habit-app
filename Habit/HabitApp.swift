1321b380a1f579a28fb9537aff01c27a87c28f76
TiniT <tinnt5@vnpay.vn>
2026-04-28 10:29:08 +0700

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by TiniT on 28/4/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
